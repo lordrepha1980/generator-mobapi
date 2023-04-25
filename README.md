f418fa23f6cd4c7a48d0fb62d44d085040895809
christoph.duengel <freelancer@christoph-duengel.de>
2023-04-25 22:59:46 +0200

    final Realease

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # generator-mobapi
+The MobAPI CLI for a full documentation about MobAPI use [MobAPI API](https://github.com/lordrepha1980/MobAPI#mobapi)
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-mobapi using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-mobapi
+```
+
+Install the latest Version from MobAPI:
+
+```bash
+yo mobapi init
+```
+
+Update your Project with the latest Version from MobAPI:
+
+```bash
+yo mobapi update
+```
+This command don't copy custom files 
+
+## License
+
+ISC © [Christoph Duengel](christoph-duengel.de)
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,10 +1,8 @@
 'use strict';
 const Generator     = require('yeoman-generator');
 const chalk         = require('chalk');
-const yosay         = require('yosay');
 const Path          = require('path');
 const Process       = require('process');
-const _dirname      = Process.cwd();
 const Os            = require('os');
 const tmpDir        = Os.tmpdir();
 const Fs            = require('fs');
@@ -18,91 +16,133 @@ module.exports = class extends Generator {
         super(args, opts)
     }
 
-    prompting() {
-        if ( this.args.length !== 1 )
-            throw new Error('Please set argument init or update (yo MobAPI install/update)')
+    mobAPI() {
+        return new Promise((resolve, reject) => {
+            Figlet("MobAPI", 
+                {
+                    font: "Larry 3D",
+                    horizontalLayout: "default",
+                    verticalLayout: "default",
+                    width: 700,
+                    whitespaceBreak: true,
+                },
+                (err, data) => {
+                    if (err) {
+                        console.log(chalk.red("Something went wrong..."));
+                        console.dir(err);
+                        reject();
+                    }
+                    console.log(chalk.blue(data));
+                    resolve();
+            });
+        })
+    }
 
-        Figlet("MobAPI", 
+    async prompting() {
+        if ( this.args.length !== 1 )
+            throw new Error('Please set argument init or update (yo MobAPI init/update)')
+
+        await this.mobAPI()
+        
+        //update or install
+        let prompts = [
             {
-                font: "Larry 3D",
-                horizontalLayout: "default",
-                verticalLayout: "default",
-                width: 700,
-                whitespaceBreak: true,
+                type: 'confirm',
+                name: 'confirm',
+                message: `MobAPI will be updated in the current directory. Continue?`,
+                default: false
             },
-            (err, data) => {
-                if (err) {
-                console.log("Something went wrong...");
-                console.dir(err);
-                return;
-                }
-                chalk.yellow(data);
-                console.log(chalk.blue(data));
-        });
+            {
+                type: 'confirm',
+                name: 'backup',
+                message: `Create a Backup of your current MobAPI?`,
+                default: true
+            }
+        ];
 
         if ( this.args[0] === 'init' ) {
-            console.log(chalk.yellow('Initializing MobAPI...'))
-
-            // const prompts = [
-            //     {
-            //         type: 'prompt',
-            //         name: 'projectDir',
-            //         message: 'Name of the project directory?',
-            //         default: Path.dirname(_dirname)
-            //     }
-            // ];
-
-            // return this.prompt(prompts).then(props => {
-            //     // To access props later use this.props.someAnswer;
-            //     this.props = props;
-            // });
+            prompts = [
+                {
+                    type: 'confirm',
+                    name: 'confirm',
+                    message: `MobAPI will be installed in the current directory. Continue?`,
+                    default: false
+                }
+            ];
         }
+
+        return this.prompt(prompts).then(props => {
+            // To access props later use this.props.someAnswer;
+            this.props = props;
+        });
     }
     
     async asyncTask() {
+        const dirname       = this.destinationPath(`./`);
         try {
-            //get git master repository
-            console.log(chalk.yellow('Downloading MobAPI...'))
-            const { data } =   await Axios.get('https://github.com/lordrepha1980/MobAPI/archive/refs/heads/master.zip', { responseType: 'arraybuffer' })
-            console.log(chalk.yellow('Downloaded MobAPI...'))
-            
-            //write repo to temp directory
-            if ( data ) {
-                //write zip file to tmp directory
-                console.log(chalk.yellow('Writing MobAPI to temp directory...'))
-                Fs.writeFileSync(`${tmpDir}/MobAPI.zip`, data);
+            if ( this.props.confirm === true ) {
+                if ( this.props.backup === true ) {
+                    const updateFiles = Fs.readdirSync(this.destinationPath(`./`))
 
-                //unpack zip file in tmp directory
-                console.log(chalk.yellow('Unpacking MobAPI...'))
-                await Extract(`${tmpDir}/MobAPI.zip`, { dir: `${tmpDir}` })
-
-                if (this.args[0] === 'init')
-                    //copy files to destination
-                    console.log(chalk.yellow('Copying MobAPI to destination...'))
-                    this.fs.copy(
-                        `${tmpDir}/MobAPI-master`,
-                        this.destinationPath(`./`)
-                    );
-
-                if (this.args[0] === 'update') {
-                    console.log(chalk.yellow('Updating MobAPI...'))
-                    const updateFiles = [
-                        'README.md',
-                        'app.js',
-                        'routes',
-                        'server/app',
-                        'server/database/',
-                    ]
-                    //copy files to destination
-                    for ( const fileName of updateFiles )
+                    //create backup directory
+                    const backupDir = `../MobAPI-backup-${Date.now()}`
+                    console.log(chalk.yellow(`Creating Backup from directory ${dirname} to parent ${backupDir}`))
+                    Fs.mkdirSync(backupDir);
+                    console.log(updateFiles)
+                    //copy files to backup directory
+                    for ( const fileName of updateFiles ) {
                         this.fs.copy(
-                            `${tmpDir}/MobAPI-master/${fileName}`,
-                            this.destinationPath(`./${fileName}`)
+                            this.destinationPath(`./${fileName}`),
+                            `${backupDir}/${fileName}`
                         );
+                    }
+                    console.log(chalk.green('Backup created...'))
                 }
-                console.log(chalk.yellow('Installing MobAPI dependencies...'))
-                this.installDependencies({ bower: false, yarn: false, npm: true });
-                console.log(chalk.green('MobAPI ready!'))
+                //get git master repository
+                console.log(chalk.yellow('Downloading MobAPI...'))
+                const { data } =   await Axios.get('https://github.com/lordrepha1980/MobAPI/archive/refs/heads/master.zip', { responseType: 'arraybuffer' })
+                console.log(chalk.yellow('Downloaded MobAPI...'))
+                
+                //write repo to temp directory
+                if ( data ) {
+                    //write zip file to tmp directory
+                    console.log(chalk.yellow('Writing MobAPI to temp directory...'))
+                    Fs.writeFileSync(`${tmpDir}/MobAPI.zip`, data);
+
+                    //unpack zip file in tmp directory
+                    console.log(chalk.yellow('Unpacking MobAPI...'))
+                    await Extract(`${tmpDir}/MobAPI.zip`, { dir: `${tmpDir}` })
+
+                    if (this.args[0] === 'init')
+                        //copy files to destination
+                        console.log(chalk.yellow('Copying MobAPI to destination...'))
+                        this.fs.copy(
+                            `${tmpDir}/MobAPI-master`,
+                            this.destinationPath(`./`)
+                        );
+
+                    if (this.args[0] === 'update') {
+                        console.log(chalk.yellow('Updating MobAPI...'))
+                        const updateFiles = [
+                            'README.md',
+                            'app.js',
+                            'routes',
+                            'server/app',
+                            'server/database/',
+                        ]
+                        //copy files to destination
+                        for ( const fileName of updateFiles ) {
+                            this.fs.copy(
+                                `${tmpDir}/MobAPI-master/${fileName}`,
+                                this.destinationPath(`./${fileName}`)
+                            );
+                        }
+                    }
+                    console.log(chalk.yellow('Installing MobAPI dependencies...'))
+                    this.installDependencies({ bower: false, yarn: false, npm: true });
+                }
+            } else {
+                console.log(chalk.red('User Abort!'))
             }
         } catch (error) {
             console.log(chalk.red('MobAPI Error!'))
